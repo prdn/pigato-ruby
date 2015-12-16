651eba1c2920f1da830fd8472bb881e36d86fbbc
Paolo Ardoino <paolo.ardoino@gmail.com>
2015-12-16 16:59:08 +0000

    Examples

--- examples/echo_client.rb
+++ examples/echo_client.rb
@@ -5,25 +5,35 @@ require "#{File.dirname(__FILE__)}/../lib/pigato.rb"
 #require "pigato"
 require 'thread'
 
-ts = []
+def start
+  ts = []
 
-[0,1,2,3,4,5,6].each do |tid|
-  ts << Thread.new {
-    client = Pigato::Client.new('tcp://127.0.0.1:55555')
-    client.start
+  (0..5).each do |tid|
+    ts << Thread.new {
+      client = Pigato::Client.new('tcp://127.0.0.1:55555')
+      client.start
 
-    requests = 10000
-    d1 = Time.now
-    requests.times do |i|
-      begin
-        client.request('echo', 'Hello world1')
+      requests = 10000
+      d1 = Time.now
+      requests.times do |i|
+        begin
+          client.request('echo', 'Hello world1')
+        end
       end
-    end
-    d2 = Time.now
-    puts "#{requests} requests/replies processed (#{(d2 - d1) * 1000} milliseconds)"
-  }
+      d2 = Time.now
+      puts "#{requests} requests/replies processed (#{(d2 - d1) * 1000} milliseconds)"
+    }
+  end
+
+  ts.each do |to|
+    to.join
+  end
 end
 
-ts.each do |to|
-  to.join
+(0..1).each do |pid|
+  pid = fork do
+    start
+  end
 end
+
+Process.waitall
--- examples/echo_worker.rb
+++ examples/echo_worker.rb
@@ -4,24 +4,34 @@ require "rubygems"
 require "#{File.dirname(__FILE__)}/../lib/pigato.rb"
 #require "pigato"
 
-ts = []
+def start
+  ts = []
 
-[0, 1, 2, 3, 4, 5, 6].each do |tid|
-  ts << Thread.new {
-    worker = Pigato::Worker.new('tcp://127.0.0.1:55555', 'echo')
-    worker.start
+  (0..5).each do |tid|
+    ts << Thread.new {
+      worker = Pigato::Worker.new('tcp://127.0.0.1:55555', 'echo')
+      worker.start
 
-    loop do
-      request = worker.recv
-      if !request.nil?
-        worker.reply request
-      else
-        sleep 0.1
+      loop do
+        request = worker.recv
+        if !request.nil?
+          worker.reply request
+        else
+          sleep 0.1
+        end
       end
-    end
-  }
+    }
+  end
+
+  ts.each do |to|
+    to.join
+  end
 end
 
-ts.each do |to|
-  to.join
+(0..1).each do |pid|
+  fork do
+    start
+  end
 end
+
+Process.waitall
